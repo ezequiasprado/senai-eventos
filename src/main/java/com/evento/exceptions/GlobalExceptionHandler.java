0a25760974e63af91e4665b3407857648da2edb2
Ezequias Prado <ezequiasmoreira@hotmail.com>
2025-02-21 19:39:26 -0300

    Criado BussinesException

---
+++ src/main/java/com/evento/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.evento.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BussinesException.class)
+    public ResponseEntity<ErrorResponse> handleValidateException
+            (BussinesException ex) {
+        return new ResponseEntity(ex.getMessage(),
+                HttpStatus.BAD_REQUEST);
+    }
+}
